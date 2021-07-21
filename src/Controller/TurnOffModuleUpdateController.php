4c63710f238d7033d9369fa16244107325e78a56
Dmitriy Ignatenko <titarx@gmail.com>
2021-07-21 20:10:07 +0400

    Project commit

---
+++ src/Controller/TurnOffModuleUpdateController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\turn_off_module_update\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+
+class TurnOffModuleUpdateController extends ControllerBase
+{
+	public function getContent()
+	{
+		return array(
+			'#type' => 'markup',
+			'#markup' => 'Отключение обновления модулей'
+		);
+	}
+	
+	public function access(AccountInterface $account)
+	{
+		// return AccessResult::allowedIf($account->hasPermission('do example things') && $this->someOtherCustomCondition());
+		return AccessResult::allowedIf(true);
+	}
+}
