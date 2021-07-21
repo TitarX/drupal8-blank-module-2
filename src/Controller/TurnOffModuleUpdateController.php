ed0394bbea6c8c43c6ec34fcd6cb3f6829ea5660
Dmitriy Ignatenko <titarx@gmail.com>
2021-07-21 20:17:02 +0400

    Fix commit

--- src/Controller/TurnOffModuleUpdateController.php
+++ src/Controller/TurnOffModuleUpdateController.php
@@ -6,19 +6,18 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
 
-class TurnOffModuleUpdateController extends ControllerBase
-{
-	public function getContent()
-	{
-		return array(
-			'#type' => 'markup',
-			'#markup' => 'Отключение обновления модулей'
-		);
-	}
-	
-	public function access(AccountInterface $account)
-	{
-		// return AccessResult::allowedIf($account->hasPermission('do example things') && $this->someOtherCustomCondition());
-		return AccessResult::allowedIf(true);
-	}
+class TurnOffModuleUpdateController extends ControllerBase {
+
+  public function getContent() {
+    return [
+      '#type' => 'markup',
+      '#markup' => 'Отключение обновления модулей',
+    ];
+  }
+
+  public function access(AccountInterface $account) {
+    // return AccessResult::allowedIf($account->hasPermission('do example things') && $this->someOtherCustomCondition());
+    return AccessResult::allowedIf(TRUE);
+  }
+
 }
